40ce04eaae70249928a22dc8a5f1a21ac753004d
Valmir-Neto <netovalmirr@gmail.com>
2025-06-19 14:35:07 -0300

    refazendo toda a lógica da minha aplicação para aplicar o que aprende.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,199 +1,112 @@
-// tarefas a serem renderizadas
 let toDoListData = [
         {id:1, description: "passear com o cachorro", checked: false},
         {id:2, description: "fazer faxina", checked: false},
         {id:3, description: "fazer compras", checked: false},
         {id:4, description: "andar de bicicleta", checked: false},
-    ];
-
-// função para deletar tarefa;
-const removeTask = (taskId) => {
-    const list = document.getElementById("lista")
-    const removeItem = document.getElementById(taskId);
-     toDoListData = toDoListData.filter(({id}) => {
-        parseInt(taskId) !== parseInt(id);
+];
+// removendo tarefas marcadas como true;
+const removeTaskTrue = () => {
+    const asRemoverItem = toDoListData.filter(({checked}) => checked);
+    toDoListData = toDoListData.filter(({checked}) => !checked);
+    asRemoverItem.forEach(({id}) => {
+        const itemAsRemoved = document.getElementById(String(id))
+        document.getElementById("lista")
+        .removeChild(itemAsRemoved);
     });
-    if(removeItem) list.removeChild(removeItem)
-    else console.log("Deu errado em alugm ponto da deleção da task");
-};
-// função que gera um novo id para cada nova task;
-const newTaskId = () => {
-    const id = toDoListData[toDoListData.length - 1]?.id;
-    return id ? id + 1 : 1;
-};
+}
+// pegando o valor do input e atualizando ele na matriz;
+const getCheckboxChecked = (event) => {
+    const name = event.target.id;
+    const [id] = name.split("-")
+    toDoListData = toDoListData.map((task) => {
+        if(parseInt(task.id) === parseInt(id)){
+            return {...task,checked:true};
+        } else return task;
+    });
+}
+// remover o item com botão;
+const removerTask = (idItem) => {
+    toDoListData = toDoListData.filter(({id}) => id != idItem);
+    const deletedItem = document.getElementById(String(idItem));
+    document.getElementById("lista").removeChild(deletedItem);
+}
+// renderiza o item;
+const getItem = (creatToDoItem) => {
+    const {wrapper, getIdForLi} = creatToDoItem;
+    const listItem = document.createElement("li");
+    listItem.id = String(getIdForLi);
+    listItem.appendChild(wrapper);
+    document.getElementById("lista").appendChild(listItem);
+}
+// peggando um novo id;
+const getNewId = () => {
+    const id = toDoListData[toDoListData.length -1]?.id;
+    return id ? id + 1 : 1
+}
+// pegando descrição;
+const getDescripition = (event) => {
+    const description =  event.target.elements.description.value;
+    return description
+}
 
-// função que pega o valor do meu input;
+// valores da nova tarefa;
 const newTaskData = (event) => {
-    const description = event.target.elements.description.value;
-    const newId = newTaskId();
-    return {newId,description};
+    const id = getNewId();
+    const description = getDescripition(event);
+    return {id, description};
 }
-    // função que limpa o input
-    const inputDOMClear = () => {
-        const inputDOMTasks = document.getElementById("description");
-        inputDOMTasks.value = "";
-    }
-    //função que cria os itens a serem renderizados
-    const createToDoItem = (ArrayElement) => {
-        // Criando os elementos que vão ser utilizados;
-        const checkBox = document.createElement("input");
-        const label = document.createElement("label");
-        const wrapper = document.createElement("div");
-        const elementId = `${ArrayElement.id}-checkBox`;
-        const button = document.createElement("button")
-
-        checkBox.type = "checkbox";
-        checkBox.id = elementId;
-        checkBox.checked = ArrayElement.checked;
-        label.htmlFor = elementId;
-        wrapper.classList.add("checkbox-label-container")
-        label.textContent = ArrayElement.description;
-        button.textContent = "X";
-        button.ariaLabel = "Deletar tarefa";
-        button.addEventListener("click", () => {
-            removeTask(ArrayElement.id)
-        });
-
-        wrapper.appendChild(checkBox);
-        wrapper.appendChild(label);
-        wrapper.appendChild(button);
-
-        return wrapper;
-    } 
-    
-    // função que recebe as informações e cria uma nova tarefa;
- //pega o evento executa a function que separa as informações adequa elas para um object e executa a função que vai montar os elemtos da lista e retona o mesmo; 
-const newTask = (event) => {
-    const objectTaskValue = {id:0, description:0, checked: false};
-    const {newId, description} = newTaskData(event)
-    objectTaskValue.id = newId;
-    objectTaskValue.description = description; 
-    const taskItem = createToDoItem(objectTaskValue);
-    toDoListData.push(objectTaskValue);
-    return taskItem;
-};
-
-// pega as informações prontas e renderiza elas
-// event necessário para pegar as informções e nomear o li;
-const taskRenderizad = (newTask, event) => {
-    const lista = document.getElementById("lista");
-        //pegando o id do item da lista e colocando ele no li
-        const {newId} = newTaskData(event);
-             // "li" que vai ir na lista;
-            const listItem = document.createElement("li");
-            listItem.id = newId;
-            const renderizedItem = newTask;
-            // colocando o item no "li";
-            listItem.appendChild(renderizedItem);
-            // colocado o "li" na lista;
-            lista.appendChild(listItem);
+// criando nova tarefa;
+const createNewTask = (event) => {
+    // prevendo o evento padrão do navegador;
+     event.preventDefault()
+    const {id, description} = newTaskData(event);
+    if (description !== ""){
+    const objectValues = {id,description,checked:false};
+    objectValues.id = id;
+    objectValues.description = description;
+    toDoListData.push(objectValues);
+    console.log(objectValues)
+    const item = creatToDoItem(objectValues);
+    getItem(item);
+    } else alert("Inseira uma tarefa válida");
 }
-const createToDoTask = (event) => {
-    event.preventDefault()
-     const input = document.getElementById("description");
-    if (input.value != ""){
-    const reserizedTask = newTask(event);
-    taskRenderizad(reserizedTask,event);
-    inputDOMClear();
-    } else {
-        alert("Insira uma tarefa!!!")
-    }
-    
-}
-// essa proppriedade serve para esperar o navegador (document) estar pronto para executar o meu programa. Isso básicamente diz o que ele vai executar assim que o doc terminar de renderizar;
-    window.onload = () => {
-        // função que previne a função default do meu navegador quando o botão for clicado
-        const listForm = document.getElementById("create-toDo-form");
-        listForm.addEventListener("submit", createToDoTask);
-        toDoListData.forEach((task) => {
-            // local onde os elementos vão ser renderizados;
-            const lista = document.getElementById("lista");
-             // "li" que vai ir na lista;
-            const listItem = document.createElement("li");
-            listItem.id = task.id;
-            const finalItem = createToDoItem(task);
-            // colocando o item no "li";
-            listItem.appendChild(finalItem);
-            // colocado o "li" na lista;
-            lista.appendChild(listItem);
-        });
 
-    }
-
-    
-/* 
-código feito com o professor
-// Para ter a certeza de que o documento está pronto para receber as informações do js é importante utilizar  o window.onload e dentro dele colocar a função que carrega o seu script do site para garantir que tudo só seja executado quando o document estiver pronto.
-const toDoListData = [
-        {id:1, description: "passear com o cachorro", checked: false},
-        {id:2, description: "fazer faxina", checked: false},
-        {id:3, description: "fazer compras", checked: false},
-        {id:4, description: "andar de bicicleta", checked: false},
-    ];
-
-
-// Montando os itens que eu preciso;
-const getCheckBox = (id, description, checked) => {
-    //criando os elementos necessários
-    const checkBox = document.createElement("input");
+// função que cria o item a ser renderizado;
+const creatToDoItem = (arrData) => {
+    const checkbox = document.createElement("input");
     const label = document.createElement("label");
     const button = document.createElement("button");
     const wrapper = document.createElement("div");
-    const checkBoxName = `${id}-checkbox`;
-    // maniupulando eles
-    checkBox.type = "checkbox";
-    checkBox.id = checkBoxName;
-    checkBox.checked = checked;
-    label.textContent = description;
-    label.htmlFor = checkBoxName;
-    wrapper.className = "checkbox-label-container";
-    //adicionando eles na wrapper
-    wrapper.appendChild(checkBox);
+    const inputName = `${arrData.id}-checkbox`;
+    const getIdForLi = arrData.id;
+    
+    checkbox.type = "checkbox";
+    checkbox.id = inputName;
+    checkbox.name = inputName;
+    checkbox.checked = arrData.checked;
+    checkbox.addEventListener("change", getCheckboxChecked);
+    label.htmlFor = inputName;
+    label.textContent = arrData.description;
+    button.textContent = "X";
+    button.ariaLabel = "deletar tarefa";
+    button.onclick = () => {removerTask(getIdForLi)}; 
+    wrapper.classList.add("checkbox-label-container");
+
+    wrapper.appendChild(checkbox);
     wrapper.appendChild(label);
-    wrapper.appendChild(button)
-    return wrapper;
-}
-// Criandoo um id para a terefa inserida no programa;
- const getNewId = () => {
-    // Essa sentença verifica se o meu array possui um valor dentro dele caso ossua ele pega o valor do ultimo e soma, se não tiver ele retorna 1 que sera o valor do priemiro id.
-    const length = toDoListData[toDoListData.length - 1]?.id;
-    return length ? length + 1 : 1
-}
-// pegando as informações do meu input e retorna elas
-const getNewTaskData = (event) => {
-    const value = event.target.elements.description.value;
-    const id = getNewId();
-    return {description:value, id: id};
-
-}
-// Função que cria e renderiza a nova tarefa;
-const createTaskListItem = (task,checkbox) => {
-    const divLista = document.getElementById("lista");
-    const tasks = document.createElement("li");
-    tasks.classList.add("div-task");
-    tasks.id = task.id
-    tasks.appendChild(checkbox);
-    divLista.appendChild(tasks);
-    toDoListData.push({id: task.id, description: task.description, checked: false})
-}
-// pegando o input da minha página e lidando com o evento padrão dele.
- const createTask = (event) => {
-    event.preventDefault();
-    const newTask = getNewTaskData(event);
-    const checkBox = getCheckBox(newTask.id,newTask.description, false);
-    createTaskListItem(newTask,checkBox);
- }
- 
-
-window.onload = () => {
-    const formValue = document.getElementById("create-toDo-form");
-    formValue.addEventListener("submit",createTask)
-    toDoListData.forEach((task) => {
-    const divLista = document.getElementById("lista");
-    const tasks = document.createElement("li");
-    tasks.classList.add("div-task");
-    tasks.appendChild(getCheckBox(task.id,task.description,task.checked));
-    divLista.appendChild(tasks);    
-    })
+    wrapper.appendChild(button);
+    return {wrapper, getIdForLi};
 };
-*/
+
+
+
+
+// Fazendo função que vai conter a script do meu site;
+window.onload = () => {
+    // pegando evento do submit
+    document.getElementById("create-toDo-form").addEventListener("submit",createNewTask);
+    toDoListData.forEach((task) => {
+        const item = creatToDoItem(task);
+        getItem(item);
+    });
+}
